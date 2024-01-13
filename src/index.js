8c994a0270f042af690a5db25c1ad4afd05d3393
Yinka09 <kadirimaranatha@yahoo.com>
2024-01-13 18:29:58 +0100

    Updated the API functionaity for the date-time, icon, conditions...

--- src/index.js
+++ src/index.js
@@ -1,9 +1,47 @@
 function displayCurrentWeather(response) {
   let currentTemp = document.querySelector("#weather-app-temp");
   let searchCity = document.querySelector("#weather-app-city");
+  let weatherDescription = document.querySelector("#description");
+  let weatherHumidity = document.querySelector("#humidity");
+  let weatherWind = document.querySelector("#wind-value");
+  let currentDateTime = document.querySelector("#date-time");
+  let date = new Date(response.data.time * 1000);
+  let weatherIcon = document.querySelector("#icon-image");
+  weatherIcon.innerHTML = `<img src="${response.data.condition.icon_url}"
+                      class="weather-app-emoji"
+                    />`;
+
   searchCity.innerHTML = response.data.city;
+  weatherDescription.innerHTML = response.data.condition.description;
+  weatherHumidity.innerHTML = `${response.data.temperature.humidity}%,`;
+  weatherWind.innerHTML = `${response.data.wind.speed}km/h`;
+  currentDateTime.innerHTML = formatDate(date);
+
+  //console.log(response.data.condition.icon);
   let apiCurrentTemp = response.data.temperature.current;
   currentTemp.innerHTML = Math.round(apiCurrentTemp);
+}
+
+function formatDate(date) {
+  let hour = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  return `${day} ${hour}:${minutes}`;
 }
 
 function apiSearchCity(city) {
